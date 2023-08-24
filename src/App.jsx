5112e59858a4233bc0c9cf9e32d4f353c98d20db
vlada_caban <vladaonly@gmail.com>
2023-08-24 19:06:41 -0400

    updated navigation and fixed background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import Nav from './components/Nav';
+import MyNav from './components/Nav';
 
 function App() {
 
   return (
     <>
-      <Nav />
+      <MyNav />
       <main className="mx-3">
       <Outlet />
       </main>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,31 @@
-import { Link } from 'react-router-dom';
-import Navbar from './UI/Navbar';
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-export default function Nav() {
+export default function MyNav() {
   return (
-    <Navbar
-      links={[
-        <Link key={1} className="nav-link text-light" to="/">
-          About Me
-        </Link>,
-        <Link key={2} className="nav-link text-light" to="/projects">
-          Projects
-        </Link>,
-        <Link key={3} className="nav-link text-light" to="/contact">
-          Contact
-        </Link>,
-      ]}
-    />
+    <Navbar expand="lg">
+      <Container>
+        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="justify-content-center"
+        >
+          <Nav>
+            <Link to="/" className="menu">
+              About Me
+            </Link>
+            <Link to="/projects" className="menu">
+              Projects
+            </Link>
+            <Link to="/contact" className="menu">
+              Contact
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
