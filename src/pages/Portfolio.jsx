52fccb3d129221980531821618537afd92f65102
vlada_caban <vladaonly@gmail.com>
2023-08-27 17:18:43 -0400

    updated projects display

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,21 +5,27 @@ import Card from 'react-bootstrap/Card';
 export default function Portfolio() {
   return (
     <div className="container pt-4 projectsWrapper">
-      {projects.map((project) => ( 
-
-        <Card key={project.image} className="projectCard">
-      <Card.Img variant="top" src={project.image} />
-      <Card.Body className="projectCardDetails">
-        <Card.Title>{project.title}</Card.Title>
-        <Card.Text>
-          To see more details click on links below.
-        </Card.Text>
-            <Card.Link href={project.deployedlink}>Deployed App Link</Card.Link>
-            <Card.Link href={project.githublink}>GitHub Repo Link</Card.Link>
-      </Card.Body>
-      </Card>
+      {projects.map((project) => (
+        <a href={project.githublink} target="blank" key={project.image} className="projectCardLink">
+          <Card className="projectCard">
+            <div className="projectCardImage">
+              <Card.Img variant="top" src={project.image} />
+            </div>
+            <hr className="lineBetween" />
+            <Card.Body className="projectCardDetails">
+              <Card.Title>{project.title}</Card.Title>
+              <Card.Text>
+                To see more details click on links below.s
+              </Card.Text>
+              <div >
+                <Card.Link href={project.deployedlink} className="projectCardFooter">Deployed App Link</Card.Link>
+                <Card.Link href={project.githublink} className="projectCardFooter">GitHub Repo Link</Card.Link>
+              </div>
+            </Card.Body>
+          </Card>
+        </a>
       ))}
-      
+
     </div>
   );
 }
