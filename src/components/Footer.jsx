377de217f8b0864359f1c25d5dafacc8a5dfa944
vlada_caban <vladaonly@gmail.com>
2023-08-27 16:25:48 -0400

    added links to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
+
+
 export default function Footer() {
     return (
         <footer className="myFooter">
-             All Rights Reserved
+                <a href="https://www.linkedin.com/in/vladacaban/" target="blank" className="footerLink"><i className="bi bi-linkedin"></i> LinkedIn</a>
 
+                <a href="https://github.com/vlada-caban" target="blank" className="footerLink"><i className="bi bi-github" ></i> GitHub</a>
+
+                <a href="https://www.instagram.com/vladaeliseeva/" target="blank" className="footerLink"><i className="bi bi-instagram"></i> Instagram</a>
         </footer>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createHashRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import './index.css';
 
 import AboutMe from './pages/AboutMe';
