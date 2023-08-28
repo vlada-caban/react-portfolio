9cfcc169bd64adc3db82b36d26a9df1621c55831
vlada_caban <vladaonly@gmail.com>
2023-08-28 16:39:48 -0400

    finally fixed the links!

--- src/projectsdata.jsx
+++ src/projectsdata.jsx
@@ -1,37 +1,45 @@
+import techtalentproject from '/assets/images/techtalentproject.jpg'
+import mealflickproject from '/assets/images/mealflickproject.jpg'
+import weatherproject from '/assets/images/weatherproject.jpg'
+import socialnetworkproject from '/assets/images/socialnetworkproject.jpg'
+import techblogproject from '/assets/images/techblogproject.jpg'
+import ecombackendproject from '/assets/images/ecombackendproject.jpg'
+
+
 export const projects = [
   {
     title: "TechTalent",
-    image: "/assets/images/techtalentproject.jpg",
+    image: techtalentproject,
     deployedlink: "https://stark-atoll-54183-6f5d9962b281.herokuapp.com/",
     githublink: "https://github.com/vlada-caban/tech-talent",
   },
   {
     title: "MealFlick",
-    image: "/assets/images/mealflickproject.jpg",
+    image: mealflickproject,
     deployedlink: "https://vlada-caban.github.io/meal-flick/",
     githublink: "https://github.com/vlada-caban/meal-flick",
   },
   {
     title: "Weather Dashboard",
-    image: "/assets/images/weatherproject.jpg",
+    image: weatherproject,
     deployedlink: "https://vlada-caban.github.io/weather-dashboard/",
     githublink: "https://github.com/vlada-caban/weather-dashboard",
   },
   {
     title: "Social Network API",
-    image: "/assets/images/socialnetworkproject.jpg",
+    image: socialnetworkproject,
     deployedlink: "https://github.com/vlada-caban/social-network-api",
     githublink: "https://github.com/vlada-caban/social-network-api",
   },
   {
     title: "TechBlog",
-    image: "/assets/images/techblogproject.jpg",
+    image: techblogproject,
     deployedlink: "https://shrouded-dusk-91199-379bcd75a9dc.herokuapp.com/",
     githublink: "https://github.com/vlada-caban/tech-blog",
   },
   {
     title: "Ecom Backend",
-    image: "/assets/images/ecombackendproject.jpg",
+    image: ecombackendproject,
     deployedlink: "https://github.com/vlada-caban/ecom-backend",
     githublink: "https://github.com/vlada-caban/ecom-backend",
   },
