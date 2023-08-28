1a120d50816ccf22be9e825fb7dacc5a64bf9802
vlada_caban <vladaonly@gmail.com>
2023-08-28 18:31:29 -0400

    added function if user clicks outside of contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,27 +3,42 @@ import { validateEmail } from '../utils/helpers';
 
 export default function Contact() {
 
-const [email, setEmail] = useState('');
-const [name, setName] = useState('');
-const [message, setMessage] = useState('');
-const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  
 
   const handleInputChange = (e) => {
     const { target } = e;
-    const inputType = target.name; 
-    const inputValue = target.value; 
+    const inputType = target.name;
+    const inputValue = target.value;
 
-    if (inputType === 'email') { 
-      setEmail(inputValue); 
-    } else if (inputType === 'name') { 
-      setName(inputValue); 
-    } else { 
-      setMessage(inputValue); 
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
     }
   };
 
+  const handleOnBlur = (e) => {
+    const { target } = e;
+    const inputValue = target.value;
+
+    if (!inputValue) {
+      setErrorMessage('Field cannot be blank');
+      return;
+    } else {
+      setErrorMessage('');
+    }
+  };
+
+
   const handleFormSubmit = (e) => {
-      e.preventDefault();
+    e.preventDefault();
 
     if (!email || !name || !message) {
       setErrorMessage('Please fill in all the fields');
@@ -34,7 +49,7 @@ const [errorMessage, setErrorMessage] = useState('');
       setErrorMessage('Email in invalid');
       return;
     }
-    
+
     setName('');
     setMessage('');
     setEmail('');
@@ -46,49 +61,52 @@ const [errorMessage, setErrorMessage] = useState('');
   return (
     <div className="container pt-4">
       <div className="contactFormSection">
-      <h2 className="contactTitle">Let's Connect!</h2>
-      <form onSubmit={handleFormSubmit} className="contactFormInputs">
-        <div className="form-group formInput">
-        <input
-         className="form-control"
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="Enter your email"
-        />
-        </div>
+        <h2 className="contactTitle">Let's Connect!</h2>
+        <form onSubmit={handleFormSubmit} className="contactFormInputs">
           <div className="form-group formInput">
-        <input
-          className="form-control"
-          value={name}
-          name="name"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Enter your name"
-        />
-        </div>
+            <input
+              className="form-control"
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              onBlur={handleOnBlur}
+              type="email"
+              placeholder="Enter your email"
+            />
+          </div>
           <div className="form-group formInput">
-        <textarea
-          className="form-control"
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          type="textarea"
-          rows="5"
-          placeholder="Enter your message here"
-        />
-        </div>
-        <button type="submit" className="contactBtn">
-          Submit
-        </button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text errorMessageContact">{errorMessage}</p>
-        </div>
-      )}
-    </div>
+            <input
+              className="form-control"
+              value={name}
+              name="name"
+              onChange={handleInputChange}
+              onBlur={handleOnBlur}
+              type="text"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="form-group formInput">
+            <textarea
+              className="form-control"
+              value={message}
+              name="message"
+              onChange={handleInputChange}
+              onBlur={handleOnBlur}
+              type="textarea"
+              rows="5"
+              placeholder="Enter your message here"
+            />
+          </div>
+          <button type="submit" className="contactBtn">
+            Submit
+          </button>
+        </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text errorMessageContact">{errorMessage}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
