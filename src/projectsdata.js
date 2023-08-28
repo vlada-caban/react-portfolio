7ff8e23c3de825f012f1a0e2f2914e8f171c17d2
vlada_caban <vladaonly@gmail.com>
2023-08-28 15:04:25 -0400

    fixed links to portfolio images

--- src/projectsdata.js
+++ src/projectsdata.js
@@ -1,37 +1,37 @@
 export const projects = [
   {
     title: "TechTalent",
-    image: "../assets/techtalentproject.jpg",
+    image: "../public/assets/images/techtalentproject.jpg",
     deployedlink: "https://stark-atoll-54183-6f5d9962b281.herokuapp.com/",
     githublink: "https://github.com/vlada-caban/tech-talent",
   },
   {
     title: "MealFlick",
-    image: "../assets/mealflickproject.jpg",
+    image: "../public/assets/images/mealflickproject.jpg",
     deployedlink: "https://vlada-caban.github.io/meal-flick/",
     githublink: "https://github.com/vlada-caban/meal-flick",
   },
   {
     title: "Weather Dashboard",
-    image: "../assets/weatherproject.jpg",
+    image: "../public/assets/images/weatherproject.jpg",
     deployedlink: "https://vlada-caban.github.io/weather-dashboard/",
     githublink: "https://github.com/vlada-caban/weather-dashboard",
   },
   {
     title: "Social Network API",
-    image: "../assets/socialnetworkproject.jpg",
+    image: "../public/assets/images/socialnetworkproject.jpg",
     deployedlink: "https://github.com/vlada-caban/social-network-api",
     githublink: "https://github.com/vlada-caban/social-network-api",
   },
   {
     title: "TechBlog",
-    image: "../assets/techblogproject.jpg",
+    image: "../public/assets/images/techblogproject.jpg",
     deployedlink: "https://shrouded-dusk-91199-379bcd75a9dc.herokuapp.com/",
     githublink: "https://github.com/vlada-caban/tech-blog",
   },
   {
     title: "Ecom Backend",
-    image: "../assets/ecombackendproject.jpg",
+    image: "../public/assets/images/ecombackendproject.jpg",
     deployedlink: "https://github.com/vlada-caban/ecom-backend",
     githublink: "https://github.com/vlada-caban/ecom-backend",
   },
