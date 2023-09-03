fa64112783d7969707fd37ca8ba1de9b504428d6
vlada_caban <vladaonly@gmail.com>
2023-09-02 20:44:51 -0400

    updated contact form to work

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,68 +1,73 @@
-import { useState } from 'react';
-import { validateEmail } from '../utils/helpers';
+// import { useState } from 'react';
+// import { validateEmail } from '../utils/helpers';
 
 export default function Contact() {
 
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [name, setName] = useState('');
+  // const [message, setMessage] = useState('');
+  // const [errorMessage, setErrorMessage] = useState('');
 
   
 
-  const handleInputChange = (e) => {
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
+  // const handleInputChange = (e) => {
+  //   const { target } = e;
+  //   const inputType = target.name;
+  //   const inputValue = target.value;
 
-    if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'name') {
-      setName(inputValue);
-    } else {
-      setMessage(inputValue);
-    }
-  };
+  //   if (inputType === 'email') {
+  //     setEmail(inputValue);
+  //   } else if (inputType === 'name') {
+  //     setName(inputValue);
+  //   } else {
+  //     setMessage(inputValue);
+  //   }
+  // };
 
-  const handleOnBlur = (e) => {
-    const { target } = e;
-    const inputValue = target.value;
+  // const handleOnBlur = (e) => {
+  //   const { target } = e;
+  //   const inputValue = target.value;
 
-    if (!inputValue) {
-      setErrorMessage('Field cannot be blank');
-      return;
-    } else {
-      setErrorMessage('');
-    }
-  };
+  //   if (!inputValue) {
+  //     setErrorMessage('Field cannot be blank');
+  //     return;
+  //   } else {
+  //     setErrorMessage('');
+  //   }
+  // };
 
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
+  // const handleFormSubmit = (e) => {
+  //   e.preventDefault();
 
-    if (!email || !name || !message) {
-      setErrorMessage('Please fill in all the fields');
-      return;
-    }
+  //   if (!email || !name || !message) {
+  //     setErrorMessage('Please fill in all the fields');
+  //     return;
+  //   }
 
-    if (!validateEmail(email)) {
-      setErrorMessage('Email in invalid');
-      return;
-    }
+  //   if (!validateEmail(email)) {
+  //     setErrorMessage('Email in invalid');
+  //     return;
+  //   }
 
-    setName('');
-    setMessage('');
-    setEmail('');
-    setErrorMessage('');
-    alert(`Message successfully sent!`);
-  };
+  //   setName('');
+  //   setMessage('');
+  //   setEmail('');
+  //   setErrorMessage('');
+  //   alert(`Message successfully sent!`);
+  // };
+
+  // width = "640" height = "781" frameborder = "0" marginheight = "0" marginwidth = "0"
 
 
   return (
     <div className="container pt-4">
       <div className="contactFormSection">
         <h2 className="contactTitle">Let's Connect!</h2>
-        <form onSubmit={handleFormSubmit} className="contactFormInputs">
+        <div className="contactFormContainer">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfbX3RgO29vdyKMFZJrnQtyxgdjElrwC9ANw5jhgvejUdoEYg/viewform?embedded=true" className="contactForm">Loading…</iframe>
+        </div>
+        {/* <form onSubmit={handleFormSubmit} className="contactFormInputs">
           <div className="form-group formInput">
             <input
               className="form-control"
@@ -105,7 +110,7 @@ export default function Contact() {
           <div>
             <p className="error-text errorMessageContact">{errorMessage}</p>
           </div>
-        )}
+        )} */}
       </div>
     </div>
   );
